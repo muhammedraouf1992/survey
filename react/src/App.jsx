562cd9b5cc5c3eaec3d438e17f27a29747242f93
muhammed <muhammedraouf92@gmail.com>
2023-08-23 16:36:56 +0300

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+import { BeakerIcon } from "@heroicons/react/24/solid";
+
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-red-400">hello world</h1>
+            <BeakerIcon className="h-6 w-6 text-blue-500" />
+        </div>
+    );
+}
+
+export default App;
